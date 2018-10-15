5f6c01e8680ecd1881afd1d650bca4b9ed82b102
alexhaec13 <39496973+alexhaec13@users.noreply.github.com>
2018-10-15 16:21:33 +0200

    V1.1.2

---
+++ ChatsApp/src/chatsapp/GestorMensajes.java
@@ -0,0 +1,12 @@
+
+package chatsapp;
+
+import java.sql.Connection;
+
+public class GestorMensajes {
+    private final static String driver = "com.mysql.jdbc.Driver";
+    private final static String servidor = "jdbc:mysql://localhost/bbdd_gestmotor";
+    private final static String user = "dam2018";
+    private final static String pass = "dam2018";
+    private Connection conexion;
+}
--- ChatsApp/src/chatsapp/Mensaje.java
+++ ChatsApp/src/chatsapp/Mensaje.java
@@ -1,6 +1,48 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package chatsapp;
 
+/**
+ *
+ * @author alexh
+ */
 public class Mensaje {
+    private String nombre;
+    private String mensaje;
+    private String fecha;
+
+    public Mensaje(String nombre, String mensaje, String fecha) {
+        this.nombre = nombre;
+        this.mensaje = mensaje;
+        this.fecha = fecha;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    
     
 }
